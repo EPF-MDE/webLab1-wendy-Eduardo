75a8fb34386b30ea36847d88c3013d50cd4fa127
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-03 17:04:31 +0200

    Lab 2 final

--- project/src/main.py
+++ project/src/main.py
@@ -2,11 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .config import settings
-<<<<<<< HEAD
-=======
 from .api.routes import api_router
 from .models import base, books, users, loans  # Importer les modèles pour Alembic
->>>>>>> e755f60eb1799c1a84315aae1c917c71281c6e90
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -23,12 +20,9 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-<<<<<<< HEAD
-=======
 # Inclusion des routes API
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
->>>>>>> e755f60eb1799c1a84315aae1c917c71281c6e90
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Library Management System API"}
